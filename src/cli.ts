e98e10b45e39466b214aa6fc1a7e6cdab0a657ee
Edwin Zaniar Putra <zaniar@nightspade.com>
2021-03-22 17:53:13 +0700

    Add help

--- src/cli.ts
+++ src/cli.ts
@@ -2,6 +2,7 @@
 
 // packages
 import mri from 'mri';
+import { name, version } from '../package.json';
 
 // local
 import { createClone } from '.';
@@ -13,6 +14,23 @@ async function main(argv_: string[]) {
 
   // we only care about the first command, anything else is whatever
   const [repoPath, dest = process.cwd()] = _;
+
+  if (!repoPath) {
+    console.log(`${name} v${version}`);
+    console.log(`Usage: ${name} <repository> [destination]`);
+    console.log('');
+    console.log('Flags:');
+    console.log(' --force               overwrite existing destination directory');
+    console.log('');
+    console.log('Examples:');
+    console.log(` ${name} github:user/repository`);
+    console.log(` ${name} github:user/repository//subdirectory/file`);
+    console.log(` ${name} gitlab:group/subgroup/repository//subdirectory/`);
+    console.log(` ${name} gitlab:user/repository#branch`);
+    console.log('');
+
+    return;
+  }
 
   await createClone(repoPath, dest, { force });
 }
